7db9d92668683da965be88cc2eae65073ea5ed93
Rahul Yadav <rahuly247@gmail.com>
2016-06-16 13:45:44 +0530

    Added REST api in Project

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+
 # It is the first place where our web page lookes
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^score/', include('score.urls')),
 ]
--- score/views.py
+++ score/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 # Whenever user requests something views are going to return them same
+
+def index(request):
+    return HttpResponse("<h1>This is Rahul Yadav !</h1>")
