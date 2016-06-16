fd2c335897440429a7a130eef6a70c81ee61acdd
Rahul Yadav <rahuly247@gmail.com>
2016-06-16 13:27:45 +0530

    Added serializer class

--- score/models.py
+++ score/models.py
@@ -1,6 +1,9 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
-# They are like tables in database.
+
+class HighScore(models.Model):
+    name = models.CharField(max_length=50)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.name
